78fdd740264b39b986c870d6c9fb0a7c977bf3db
Tomáš Řehák <tomrehak353@gmail.com>
2025-01-14 21:47:42 +0100

    update

--- modules/tom.nix
+++ modules/tom.nix
@@ -1,19 +1,19 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tom = {
     isNormalUser = true;
     description = "Tom";
     extraGroups = [ "networkmanager" "wheel" "scanner" "lp" ];
-    packages = with pkgs; [
-      ungoogled-chromium
-      discord
-      krita inkscape gimp
-      openscad blender prusa-slicer
-      dwarf-fortress mindustry #libremines
-      vscode-fhs
-      termusic yt-dlp ffmpeg # music player + youtube downloader
-      github-desktop
+    packages = [
+      pkgs.ungoogled-chromium
+      pkgs.discord
+      pkgs.krita pkgs.inkscape pkgs.gimp
+      pkgs.openscad pkgs.blender pkgs-unstable.prusa-slicer
+      pkgs.dwarf-fortress pkgs.mindustry #libremines
+      pkgs.vscode-fhs
+      pkgs.termusic pkgs-unstable.yt-dlp pkgs.ffmpeg # music player + youtube downloader
+      pkgs.github-desktop
     ];
   };
   
