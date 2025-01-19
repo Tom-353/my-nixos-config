992b0ff57bcaa903f5e793f2a985951a4ca222a3
Tomáš Řehák <tomrehak353@gmail.com>
2025-01-19 19:13:07 +0100

    nvf

--- flake.nix
+++ flake.nix
@@ -6,23 +6,36 @@
     unstablepkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = { self, nixpkgs, unstablepkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      lib = nixpkgs.lib;
-      pkgs = nixpkgs.legacyPackages.${system};
-      pkgs-unstable = unstablepkgs.legacyPackages.${system};
-    in {
+  outputs = {
+    self,
+    nixpkgs,
+    unstablepkgs,
+    home-manager,
+    nvf,
+    ...
+  }: let
+    system = "x86_64-linux";
+    lib = nixpkgs.lib;
+    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs-unstable = unstablepkgs.legacyPackages.${system};
+    customNeovim = nvf.lib.neovimConfiguration {
+      inherit pkgs;
+      modules = [./nvf/configuration.nix];
+    };
+  in {
+    packages.${system}.my-neovim = customNeovim.neovim;
     nixosConfigurations = {
       "nixos-pc" = lib.nixosSystem {
         inherit system;
         #inherit unstablepkgs;
-        specialArgs = { inherit pkgs-unstable; };
+        specialArgs = {inherit pkgs-unstable;};
         modules = [
           ./hosts/nixos-pc/configuration.nix
           ./modules/tom.nix
+          {environment.systemPackages = [customNeovim.neovim];}
           ./modules/management.nix
           ./modules/hyprland.nix
           ./modules/gaming.nix
@@ -32,7 +45,7 @@
       };
       "nixos-t440" = lib.nixosSystem {
         inherit system;
-        specialArgs = { inherit pkgs-unstable; };
+        specialArgs = {inherit pkgs-unstable;};
         modules = [
           ./hosts/nixos-t440/configuration.nix
           ./modules/tom.nix
@@ -46,9 +59,8 @@
     homeConfigurations = {
       "tom" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./home/tom/home.nix ];
+        modules = [./home/tom/home.nix];
       };
     };
-
   };
 }
